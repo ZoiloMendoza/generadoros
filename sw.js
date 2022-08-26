b476f7709f340e7d781fda0175c22502ab5653d9
ZoiloMendoza <zoilomm@hotmail.com>
2022-08-26 13:24:50 -0500

    modificando app.js

--- sw.js
+++ sw.js
@@ -14,10 +14,7 @@ const APP_SHELL = [
 ];
 
 const APP_SHELL_INMUTABLE = [
-    'https://fonts.googleapis.com/css?family=Quicksand:300,400',
-    'https://fonts.googleapis.com/css?family=Lato:400,300',
-    'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
-    'css/animate.css',
+
 ];
 
 self.addEventListener('install', e =>{ //evento de instalación.
